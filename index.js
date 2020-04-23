7df010a11bc91b0225963582ff2a4d30705112a0
Asvarox <asvarox93@gmail.com>
2020-04-23 20:11:49 +0200

    Create simple ApolloServer

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hellow world");
+const { ApolloServer } = require("apollo-server");
+
+const typeDefs = `
+type Query{
+    greeting: String
+}
+`;
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen({ port: 4000 }).then((result) => console.log(result.url));
