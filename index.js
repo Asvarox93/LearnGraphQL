f3f3e65440e402c4d300fc82bd924f360fa9d2f6
Asvarox <asvarox93@gmail.com>
2020-04-25 15:53:43 +0200

    Add random dice throw

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { ApolloServer, gql } = require("apollo-server");
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 const typeDefs = gql`
   type Query {
     greeting: String
@@ -8,20 +8,44 @@ const typeDefs = gql`
     firstName: String
     email: String
     pets: [String]
+    randomDiceThrow: Int
+    fewRandomDiceThrows: [Int]
+    counter: Int
   }
 `;
 
-const data = {
-  greeting: "Hello World!",
-  interestingUrls: ["https://google.com", "https://sbialek.com"],
-  firstName: "John",
-  email: "john@example.com",
-  pets: ["Mittens", "Doggo", "Birb"],
+const rootValue = () => {
+  rootValue.calledTimes++;
+
+  const getRandomDiceThrow = (sides) => Math.ceil(Math.random() * sides);
+  const getFewRandomDiceThrows = (sides) => {
+    const throwsNumber = Math.floor(Math.random() * (7 - 3 + 1)) + 3;
+    const throwArray = [];
+
+    for (let i = 0; i < throwsNumber; i++) {
+      throwArray.push(getRandomDiceThrow(sides));
+    }
+    return throwArray;
+  };
+
+  const data = {
+    greeting: "Hello World!",
+    interestingUrls: ["https://google.com", "https://sbialek.com"],
+    firstName: "John",
+    email: "john@example.com",
+    pets: ["Mittens", "Doggo", "Birb"],
+    randomDiceThrow: getRandomDiceThrow(6),
+    fewRandomDiceThrows: getFewRandomDiceThrows(6),
+    counter: rootValue.calledTimes,
+  };
+
+  return data;
 };
+rootValue.calledTimes = 0;
 
 const server = new ApolloServer({
   typeDefs,
-  rootValue: data,
+  rootValue,
   playground: true,
   introspection: true,
 });
